780c5071901f6569a203b8448e0eb5c0dae1e86f
Caleb McCarthy <caleb.a.mccarthy@gmail.com>
2020-02-15 07:56:32 -0600

    Add a last score

--- src/databasecreator.cpp
+++ src/databasecreator.cpp
@@ -122,6 +122,25 @@ void databasecreator::createflashcardsTable(){
 		std::cout << "exception: " << e.what() << std::endl;
 	}
 }
+
+
+void databasecreator::createflashcardcarddataTable(){
+	try {
+		std::string tablename = "flashcarddata";
+		std::string dest = "cedict.db3";
+		std::cout << "Creating "<< tablename <<" table" << std::endl;
+		SQLite::Database    db(dest, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
+		SQLite::Transaction transaction(db);
+		db.exec("DROP TABLE IF EXISTS "+tablename);
+		db.exec("CREATE TABLE " + tablename + "(id INTEGER PRIMARY KEY,flashcarddeckid INTEGER,cedictid INTEGER,LastEditTime DATETIME DEFAULT CURRENT_TIMESTAMP,lastanswercorrect INTEGER,FOREIGN KEY(flashcarddeckid) REFERENCES flashcarddeck(id), FOREIGN KEY(cedictid) REFERENCES cedict(id))");
+		std::cout << "Finished Creating "+tablename+" table "<< std::endl;
+		transaction.commit();
+	}
+
+	catch (std::exception & e) {
+		std::cout << "exception: " << e.what() << std::endl;
+	}
+}
 void databasecreator::createEnumTable(){
 	try {
 		std::string tablename = "enums";
@@ -139,25 +158,6 @@ void databasecreator::createEnumTable(){
 		std::cout << "exception: " << e.what() << std::endl;
 	}
 }
-
-void databasecreator::createflashcardcarddataTable(){
-	try {
-		std::string tablename = "flashcarddata";
-		std::string dest = "cedict.db3";
-		std::cout << "Creating "<< tablename <<" table" << std::endl;
-		SQLite::Database    db(dest, SQLite::OPEN_READWRITE | SQLite::OPEN_CREATE);
-		SQLite::Transaction transaction(db);
-		db.exec("DROP TABLE IF EXISTS "+tablename);
-		db.exec("CREATE TABLE " + tablename + "(id INTEGER PRIMARY KEY,flashcarddeckid INTEGER,cedictid INTEGER,LastEditTime DATETIME DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY(flashcarddeckid) REFERENCES flashcarddeck(id), FOREIGN KEY(cedictid) REFERENCES cedict(id))");
-		std::cout << "Finished Creating "+tablename+" table "<< std::endl;
-		transaction.commit();
-	}
-
-	catch (std::exception & e) {
-		std::cout << "exception: " << e.what() << std::endl;
-	}
-}
-
 
 void databasecreator::createDefinitionsTable() {
 	try {
